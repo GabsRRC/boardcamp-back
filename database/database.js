8d4109b4cab09ebbe591edef01a2e2b0891ea3af
GabsRRC <gabriellerrc@gmail.com>
2022-07-25 06:01:24 -0300

    Heroku commit

--- database/database.js
+++ database/database.js
@@ -5,14 +5,23 @@ dotenv.config();
 
 const {Pool} = pg;
 
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+// const db = new Pool({
+//   connectionString: process.env.DATABASE_URL
+// });
 
-if(process.env.MODE === "PROD") {
-  db.ssl = {
-    rejectUnauthorized: false
+// if(process.env.MODE === "PROD") {
+//   db.ssl = {
+//     rejectUnauthorized: false
+//   }
+// }
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
   }
 }
+
+const db = new Pool(databaseConfig);
 
 export default db;
